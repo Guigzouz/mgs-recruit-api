a0333053784adcd62b660cbe50a17745460eb4a7
Guigzouz <benard.guillaume0@gmail.com>
2023-12-03 18:48:22 +0100

    init db + docker & readme

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const db = require("./config/db");
+
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
+db();
 
 app.listen(port, () => console.log(`Express app running on port ${port}!`));
---
+++ config/db.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const MONGODB_URL = process.env.MONGODB_URL;
+
+const db = async () => {
+  try {
+    const con = await mongoose.connect(MONGODB_URL);
+    console.log(`mongodb connected: ${con.connection.host}`);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = db;
